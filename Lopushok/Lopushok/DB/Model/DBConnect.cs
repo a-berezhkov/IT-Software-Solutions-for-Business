ae50c83d85387b7acabe565677aa278f68a7565a
Артём Коршунов <korshun.herooo@yandex.ru>
2021-12-06 21:54:31 +0300

    V.0.1. МОДЕЛЬ БАЗЫ ДАННЫХ - База данных - 1. Разработана база данных Lopushok 2. Добавлена модель БД 3. Добавлен SQL-скрипт БД (для создания БД на локальном компьютере) 4. Добавлен Excel файл с данными для БД (на всякий случай) 5. Добавлен класс DBConnect для удобной работы с таблицами БД

---
+++ Lopushok/Lopushok/DB/Model/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lopushok.DB.Model
+{
+    public class DBConnect
+    {
+        public static LopushokEntities db = new LopushokEntities();
+    }
+}
