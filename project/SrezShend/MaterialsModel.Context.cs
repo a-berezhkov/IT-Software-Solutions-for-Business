58628ea015eae53e5c9ea42b776c66c6ec358809
DjNik <nikitos1235@gmail.com>
2021-12-07 12:02:55 +0300

    Finally, USE ARTEM Script, added localdb support

--- project/SrezShend/MaterialsModel.Context.cs
+++ project/SrezShend/MaterialsModel.Context.cs
@@ -38,6 +38,6 @@ namespace SrezShend
         public virtual DbSet<ProductType> ProductType { get; set; }
         public virtual DbSet<Shop> Shop { get; set; }
         public virtual DbSet<Supplier> Supplier { get; set; }
-        public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
+        public virtual DbSet<SupplierType> SupplierType { get; set; }
     }
 }
--- project/SrezShend/SupplierType.cs
+++ project/SrezShend/SupplierType.cs
@@ -12,12 +12,9 @@ namespace SrezShend
     using System;
     using System.Collections.Generic;
     
-    public partial class sysdiagrams
+    public partial class SupplierType
     {
-        public string name { get; set; }
-        public int principal_id { get; set; }
-        public int diagram_id { get; set; }
-        public Nullable<int> version { get; set; }
-        public byte[] definition { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
     }
 }
